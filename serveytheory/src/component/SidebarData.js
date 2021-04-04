8f9553d8e37521b94330950abd2bd20fc28a7fea
ParkJaeyeon0308 <s2019w10@e-mirim.hs.kr>
2021-04-04 15:03:06 +0900

    commit

---
+++ serveytheory/src/component/SidebarData.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as BiIcons from "react-icons/bi";
+import * as GrIcons from "react-icons/gr";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path:'/',
+        icon:<AiIcons.AiOutlineHome/>,
+        cName: 'menu-text'
+    },
+    {
+        title: 'Login/Logout',
+        path:'/log',
+        icon:<BiIcons.BiLogIn/>,
+        cName: 'menu-text'
+    },
+    {
+        title: 'My Result',
+        path:'/result',
+        icon:<GrIcons.GrDocumentVerified/>,
+        cName: 'menu-text'
+    },
+    {
+        title: 'Account',
+        path:'/account',
+        icon:<IoIcons.IoIosPerson/>,
+        cName: 'menu-text'
+    }
+];
