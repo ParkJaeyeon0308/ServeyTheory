fbe573e4ecc3b7b761a7f88e7776f0fe0b7bc8d5
ParkJaeyeon0308 <wowoyeoni@gmail.com>
2021-05-23 17:58:06 +0900

    에러 수정

--- serveytheory/src/component/SidebarData.js
+++ serveytheory/src/component/SidebarData.js
@@ -2,36 +2,6 @@ import React from "react";
 
 export const SidebarData = [
     {
-<<<<<<< HEAD
-        title: '홈',
-        path:'/',
-        cName: 'nav-text'
-    },
-    {
-        title: '로그인/로그아웃',
-        path:'/login',
-        cName: 'nav-text'
-    },
-    {
-        title: '내 설문 몰아보기',
-        path:'/result',
-        cName: 'nav-text'
-    },
-    {
-        title: '계정설정',
-        cName:'fixnav-text'
-    
-    },
-    {
-        title: '암호변경',
-        path:'/Pass_change',
-        cName: 'mini-nav-text'
-    },
-    {
-        title: '회원탈퇴',
-        path:'/quit_account',
-        cName: 'mini-nav-text'
-=======
         title: "홈",
         path: "/",
         // icon:<AiIcons.AiOutlineHome color="#ffffff"/>,
@@ -66,7 +36,6 @@ export const SidebarData = [
         path: "/quit_account",
         // icon:<BsIcons.BsFillGridFill color="#ffffff"/>,
         cName: "mini-nav-text"
->>>>>>> 16fe305f7e287c8e025a1428c652bbf41b2e746c
     },
     {
         title: "Developer Intro.",
