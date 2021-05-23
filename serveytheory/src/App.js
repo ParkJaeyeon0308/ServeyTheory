562ac4ce8571294eedc4bceba8b45033b1ee2468
맹수연 <sooyeon030908@gmail.com>
2021-05-23 16:53:39 +0900

    결과 페이지 Route 추가

--- serveytheory/src/App.js
+++ serveytheory/src/App.js
@@ -4,13 +4,13 @@ import NavBar from "./component/NavBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Result from "./pages/Result";
 import Account from "./pages/Account";
 import PassChange from "./pages/PassChange";
 import QuitAccount from "./pages/QuitAccount";
 import SignUp from "./pages/SignUp";
 
 import Question from "./pages/Question/question";
+import Result from "./pages/Result/result";
 import Developer_intro from "./pages/Developer_intro/developer_intro";
 
 // 여기서 페이지 이동을 담당
@@ -24,12 +24,12 @@ function App() {
                     <Route path="/" exact component={Home} />
                     <Route path="/login" exact component={Login} />
                     <Route path="/signup" component={SignUp} />
-                    <Route path="/result" component={Result} />
                     <Route path="/account" component={Account} />
                     <Route path="/pass_change" component={PassChange} />
                     <Route path="/quit_account" component={QuitAccount} />
 
                     <Route path="/question" component={Question} />
+                    <Route path="/result" component={Result} />
                     <Route
                         path="/developer_intro"
                         component={Developer_intro}
