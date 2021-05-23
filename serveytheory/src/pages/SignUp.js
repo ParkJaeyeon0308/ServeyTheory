794f43b7f8aa2649a689f51ad619a66d21ad6c8e
ParkJaeyeon0308 <wowoyeoni@gmail.com>
2021-05-23 19:17:42 +0900

    회원가입 대강 디자인

--- serveytheory/src/pages/SignUp.js
+++ serveytheory/src/pages/SignUp.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom'
-import { Container,Row,Col } from 'react-bootstrap';
+import { InputGroup, FormControl,Button } from 'react-bootstrap';
 import './SignUp.css';
 
 const clientId = "OAuth Web Client ID";
@@ -20,14 +20,27 @@ class SignUp extends Component{
     }; render(){ 
         return (
         <center>
-        <h2 className="LoginHeader"> 회원가입 </h2>
-        <div style={{width:400}}><input type="nickname" id="inputNick" className="form-control" placeholder="닉네임" name="nickname" style={{marginBottom:20}} onChange={this.handleChange}/>
-        <button className="btn btn-lg btn-block" type="button" onClick={this.handleOnClick}>중복확인</button></div>
-         <input type="id" id="inputid" className="form-control" placeholder="ID" name="id" style={{marginBottom:20}} onChange={this.handleChange}/>
-          <input type="password" id="inputPhone" className="form-control" placeholder="비밀번호" name="phone" style={{marginBottom:20}} onChange={this.handleChange}/>
-            <Link to="./Login">
-           <button className="btn btn-lg btn-block" type="button" style={{backgroundColor:"#fffadd", color:"#102f57"}} onClick={this.handleOnClick}> 회원가입 </button> 
-           </Link></center>
+        <div id="body">
+        <h1 className="LoginHeader"> 회원가입 </h1>
+        <br></br>
+        <InputGroup className="mb-3" id="inputs">
+        <FormControl
+        style={{fontSize:25}}
+          placeholder="닉네임"
+          aria-label="닉네임"
+          aria-describedby="basic-addon2"
+        />
+        <InputGroup.Append>
+          <Button variant="outline-secondary" id="button">중복체크</Button>
+        </InputGroup.Append>
+      </InputGroup>
+      <input type="id" id="inputid" className="form-control" placeholder="ID" name="id" id="id_inputs" onChange={this.handleChange}/>
+      <input type="password" id="inputPW" className="form-control" placeholder="비밀번호" name="PW" id="pw_inputs"  onChange={this.handleChange}/>
+      <input type="password" id="inputPWcheck" className="form-control" placeholder="비밀번호 확인" name="PW_check" id="pw_inputs"  onChange={this.handleChange}/>
+      <Link to="./Login">
+        <button className="btn btn-lg btn-block" type="button" id="buttons" onClick={this.handleOnClick}> 회원가입 </button></Link>
+        </div>
+      </center>
            ); 
         }; 
     } export default SignUp;
