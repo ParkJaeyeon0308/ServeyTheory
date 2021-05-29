67a115d17deca911e2c1fe2c800cc695b8075dc2
맹수연 <sooyeon030908@gmail.com>
2021-05-29 17:01:05 +0900

    progress bar 프론트 구현

--- serveytheory/src/pages/Question/question_3.js
+++ serveytheory/src/pages/Question/question_3.js
@@ -9,31 +9,30 @@ class question extends React.Component {
         const btn2 = this.props.btn2;
         const btn3 = this.props.btn3;
 
-        const progress = this.props.progress;
-
         return (
             <div className="Question">
-                <p className="progress_text">{progress}% 진행 중</p>
-                <p className="question_text">{question_title}</p>
                 <div className="container">
-                    <div>
-                        <img id="image" src="../images/지브리.png" />
+                    <progress id="progressbar" value="10" max="100"></progress>
+                    <div
+                        className="question_title"
+                        // html 형태로 렌더링 허용
+                        dangerouslySetInnerHTML={{ __html: question_title }}
+                    ></div>
+                    <div className="buttons">
+                        <Button onClick={() => console.log("클릭 이벤트 처리")}>
+                            {btn1}
+                        </Button>
+                        <br />
+
+                        <Button onClick={() => console.log("클릭 이벤트 처리")}>
+                            {btn2}
+                        </Button>
+                        <br />
+
+                        <Button onClick={() => console.log("클릭 이벤트 처리")}>
+                            {btn3}
+                        </Button>
                     </div>
-                </div>
-                <div className="buttons">
-                    <Button onClick={() => console.log("클릭 이벤트 처리")}>
-                        {btn1}
-                    </Button>
-                    <br />
-
-                    <Button onClick={() => console.log("클릭 이벤트 처리")}>
-                        {btn2}
-                    </Button>
-                    <br />
-
-                    <Button onClick={() => console.log("클릭 이벤트 처리")}>
-                        {btn3}
-                    </Button>
                 </div>
             </div>
         );
@@ -43,11 +42,10 @@ class question extends React.Component {
 // 기본 props 값
 question.defaultProps = {
     question_title:
-        "Q1. 붕어빵이 먹고싶어서 사러나가려고 한다. 당신이 선택한 가게는?",
+        "Q1. 붕어빵이 먹고싶어서 사러나가려고 한다.<br/> 당신이 선택한 가게는?",
     btn1: "집이랑 가까운 곳",
     btn2: "내가 좋아하는 슈크림 붕어빵이 있는 곳",
-    btn3: "귀찮아~ 집에 있을래",
-    progress: 0
+    btn3: "귀찮아~ 집에 있을래"
 };
 
 export default question;
