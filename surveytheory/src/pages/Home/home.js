88818ba556e6aa2096bfa1bb64df8d1cc82bf5c8
맹수연 <sooyeon030908@gmail.com>
2021-05-29 23:31:20 +0900

    3 3 1 박스 배치

--- surveytheory/src/pages/Home/home.js
+++ surveytheory/src/pages/Home/home.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
@@ -6,6 +8,14 @@ import "./slider.css";
 
 export default class Home extends Component {
     render() {
+        // img style settings
+        const img_style = {
+            width: "30rem",
+            height: "20rem",
+            margin: "0 auto"
+        };
+
+        // slider settings
         const settings = {
             dots: true, //넘겨지는 점 보여지게
             arrows: true,
@@ -19,42 +29,49 @@ export default class Home extends Component {
             <div className="container">
                 <Slider {...settings} className="slide">
                     <div>
-                        <img
-                            src="/images/지브리.png"
-                            style={{ width: "30rem", height: "20rem" }}
-                        />
+                        <img src="/images/지브리.png" style={img_style} />
                     </div>
                     <div>
                         <img
                             src="/images/스코틀랜드 격자.jpg"
-                            style={{ width: "30rem", height: "20rem" }}
+                            style={img_style}
                         />
                     </div>
                     <div>
-                        <img
-                            src="/images/아가일 체크.jpg"
-                            style={{ width: "30rem", height: "20rem" }}
-                        />
+                        <img src="/images/아가일 체크.jpg" style={img_style} />
                     </div>
                     <div>
-                        <img
-                            src="/images/보라하늘.png"
-                            style={{ width: "30rem", height: "20rem" }}
-                        />
+                        <img src="/images/보라하늘.png" style={img_style} />
                     </div>
                     <div>
-                        <img
-                            src="/images/노란 체크.jpg"
-                            style={{ width: "30rem", height: "20rem" }}
-                        />
+                        <img src="/images/노란 체크.jpg" style={img_style} />
                     </div>
                     <div>
                         <img
                             src="/images/스코틀랜드 격자.jpg"
-                            style={{ width: "30rem", height: "20rem" }}
+                            style={img_style}
                         />
                     </div>
                 </Slider>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+                <div className="box"></div>
+
+                {/* 빈 박스 */}
+                <div className="box"></div>
+                <div className="box"></div>
+                <Link to="./SignUp">
+                    <button type="button" className="more">
+                        더보기
+                    </button>
+                </Link>
             </div>
         );
     }
