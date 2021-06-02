d35103d06e7f1264986e70d3ae73530ed818efa4
Shinipsae <s2019s13@e-mirim.hs.kr>
2021-06-02 23:07:44 +0900

    fixed error

--- surveytheory/src/pages/Question/Q1/question1_1.js
+++ surveytheory/src/pages/Question/Q1/question1_1.js
@@ -1,5 +1,4 @@
 import React from "react";
-import React, { useState } from "react";
 import "../question.css";
 import Button from "../../../component/Button";
 
@@ -7,22 +6,9 @@ class question extends React.Component {
     render() {
         const question_title = this.props.question_title;
         const btn1 = this.props.btn1;
+        const btn2 = this.props.btn2;
 
-        
-        const [number, setNumber] = useState(1);
-
-  const increaseNumber = () => { // number의 값을 증가시키는 함수
-    setNumber(number + 1);
-    // setNumber(prevNumber => prevNumber + 1);
-    // 다음과 같이 현재 number의 값을 불러와서 증가 시켜주는 방법도 있습니다 :)
-  };
-
-  const decreaseNumber = () => { // number의 값을 감소시키는 함수
-    setNumber(number - 1);
-    // setNumber(prevNumber => prevNumber - 1);
-    // 다음과 같이 현재 number의 값을 불러와서 감소 시켜주는 방법도 있습니다 :)
-  }
-    return (
+        return (
             <div className="Question">
                 <div className="container">
                     <progress id="progressbar" value="10" max="100"></progress>
@@ -34,9 +20,7 @@ class question extends React.Component {
                     <div className="buttons">
                         <Button
                             onClick={() =>
-                                this.props.history.push(decreaseNumber()),
-                                this.props.history.push("/question1_2"),
-                                console.log()
+                                this.props.history.push("/question1_3")
                             }
                         >
                             <div
@@ -49,9 +33,7 @@ class question extends React.Component {
 
                         <Button
                             onClick={() =>
-                                this.props.history.push(increaseNumber()),
-                                this.props.history.push("/question1_2"),
-                                console.log()
+                                this.props.history.push("/question1_3")
                             }
                         >
                             <div
