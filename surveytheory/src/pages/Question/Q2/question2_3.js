3b4762bad69ac563d8db2076c3ee58214a73c1cb
Shinipsae <s2019s13@e-mirim.hs.kr>
2021-06-02 23:24:03 +0900

    update progress-bar

--- surveytheory/src/pages/Question/Q2/question2_3.js
+++ surveytheory/src/pages/Question/Q2/question2_3.js
@@ -11,7 +11,7 @@ class question extends React.Component {
         return (
             <div className="Question">
                 <div className="container">
-                    <progress id="progressbar" value="10" max="100"></progress>
+                    <progress id="progressbar" value="42" max="100"></progress>
                     <div
                         className="question_title"
                         // html 형태로 렌더링 허용
