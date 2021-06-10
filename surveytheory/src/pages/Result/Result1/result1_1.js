60d9ac6752cb7790b77fbc6e0c3e2642fb6173e9
suyeon <s2019w27@e-mirim.hs.kr>
2021-06-10 14:02:26 +0900

    result1~5 route 연결 및 button 추가

--- surveytheory/src/pages/Result/Result1/result1_1.js
+++ surveytheory/src/pages/Result/Result1/result1_1.js
@@ -1,21 +1,49 @@
 import React from "react";
-import "./result.css";
+import "../result.css";
 
-class result extends React.Component {
+class result1_1 extends React.Component {
     render() {
-        const type_name = this.props.type_name;
-        const imgUrl = "/images/" + type_name + ".png";
+        const subject_name = this.props.subject_name;
+        const imgUrl = "/images/" + subject_name + ".png";
+        const main_context = this.props.main_context;
         const sub_context = this.props.sub_context;
+
+        const saveBtn = "결과저장";
+        const mainBtn = "메인으로";
         return (
             <div className="result">
                 <div className="container">
-                    <div className="type">[ {type_name} ]</div>
-                    <img className="type-img" src={imgUrl} alt={type_name} />
+                    <div
+                        className="subject_name"
+                        // html 형태로 렌더링 허용
+                        dangerouslySetInnerHTML={{ __html: subject_name }}
+                    ></div>
+                    <img className="type-img" src={imgUrl} alt={subject_name} />
+                    <div
+                        className="main_context"
+                        // html 형태로 렌더링 허용
+                        dangerouslySetInnerHTML={{ __html: main_context }}
+                    ></div>
                     <div
                         className="sub_context"
                         // html 형태로 렌더링 허용
                         dangerouslySetInnerHTML={{ __html: sub_context }}
                     ></div>
+
+                    <div className="buttons">
+                        <button
+                            className="save_btn"
+                            onClick={() => this.props.history.push("/collect")}
+                        >
+                            {saveBtn}
+                        </button>
+                        <button
+                            className="main_btn"
+                            onClick={() => this.props.history.push("/")}
+                        >
+                            {mainBtn}
+                        </button>
+                    </div>
                 </div>
             </div>
         );
@@ -23,12 +51,11 @@ class result extends React.Component {
 }
 
 // 기본 props 값
-result.defaultProps = {
-    type_name: "당신은 웹과 더 잘맞는 성향이시군요?",
+result1_1.defaultProps = {
+    subject_name: "웹, 앱 뭐가 더 잘 맞을까?",
+    main_context: "당신은 웹과 더 잘 맞는 성향이시군요?",
     sub_context:
-        "웹이란? 월드 와이드 웹의 준말로 크롬과같은 브라우저에서" +
-        "동작하는 홈페이지와 같은 서비스예요. </br> 접근성이 낮고 상대적으로 저렴하게 개발할 수있어요.  </br> "+
-        "웹을 더 많이 사용하고 간편한 것을 좋아하는 당신은 웹 개발을 할 때 더 흥미를 느낄 수 있을거예요!  </br> "
+        "웹이란? 월드 와이드 웹의 준말로 크롬과같은 브라우저에서 동작하는 홈페이지와 같은 서비스예요. 접근성이 낮고 상대적으로 저렴하게 개발할 수있어요.  </br> 웹을 더 많이 사용하고 간편한 것을 좋아하는 당신은 웹 개발을 할 때 더 흥미를 느낄 수 있을거예요!  </br> "
 };
 
-export default result;
+export default result1_1;
--- surveytheory/src/pages/Result/Result3/result3_3.js
+++ surveytheory/src/pages/Result/Result3/result3_3.js
@@ -1,21 +1,49 @@
 import React from "react";
-import "./result.css";
+import "../result.css";
 
-class result extends React.Component {
+class result3_3 extends React.Component {
     render() {
-        const type_name = this.props.type_name;
-        const imgUrl = "/images/" + type_name + ".png";
+        const subject_name = this.props.subject_name;
+        const imgUrl = "/images/" + subject_name + ".png";
+        const main_context = this.props.main_context;
         const sub_context = this.props.sub_context;
+
+        const saveBtn = "결과저장";
+        const mainBtn = "메인으로";
         return (
             <div className="result">
                 <div className="container">
-                    <div className="type">[ {type_name} ]</div>
-                    <img className="type-img" src={imgUrl} alt={type_name} />
+                    <div
+                        className="subject_name"
+                        // html 형태로 렌더링 허용
+                        dangerouslySetInnerHTML={{ __html: subject_name }}
+                    ></div>
+                    <img className="type-img" src={imgUrl} alt={subject_name} />
+                    <div
+                        className="main_context"
+                        // html 형태로 렌더링 허용
+                        dangerouslySetInnerHTML={{ __html: main_context }}
+                    ></div>
                     <div
                         className="sub_context"
                         // html 형태로 렌더링 허용
                         dangerouslySetInnerHTML={{ __html: sub_context }}
                     ></div>
+
+                    <div className="buttons">
+                        <button
+                            className="save_btn"
+                            onClick={() => this.props.history.push("/collect")}
+                        >
+                            {saveBtn}
+                        </button>
+                        <button
+                            className="main_btn"
+                            onClick={() => this.props.history.push("/")}
+                        >
+                            {mainBtn}
+                        </button>
+                    </div>
                 </div>
             </div>
         );
@@ -23,13 +51,11 @@ class result extends React.Component {
 }
 
 // 기본 props 값
-result.defaultProps = {
-    type_name: "3학년",
+result3_3.defaultProps = {
+    subject_name: "정신학년테스트",
+    main_context: "3학년",
     sub_context:
-        "당신은 진정한 미림인입니다. 혹시 3학년인가요? </br>" +
-        "프로젝트와 개발 경험들로 취업까지 하셨을지 모르겠네요.</br>" +
-        "당신의 앞길을 응원합니다!</br>"
-        
+        "당신은 진정한 미림인입니다. 혹시 3학년 인가요? </br> 프로젝트와 개발 경험들로 취업까지 하셨을 지 모르겠네요. 당신의 앞길을 응원합니다!"
 };
 
-export default result;
+export default result3_3;
