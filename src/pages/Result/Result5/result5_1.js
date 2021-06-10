b45a1c32e383aa55de3d0f3d765e057709fd1789
suyeon <s2019w27@e-mirim.hs.kr>
2021-06-10 14:16:16 +0900

    server와 client 통일

--- src/pages/Result/Result5/result5_1.js
+++ src/pages/Result/Result5/result5_1.js
@@ -1,21 +1,49 @@
 import React from "react";
-import "./result.css";
+import "../result.css";
 
-class result extends React.Component {
+class result5_1 extends React.Component {
     render() {
-        const type_name = this.props.type_name;
-        const imgUrl = "/images/" + type_name + ".png";
+        const subject_name = this.props.subject_name;
+        const imgUrl = "/images/" + subject_name + ".png";
+        const main_context = this.props.main_context;
         const sub_context = this.props.sub_context;
+
+        const saveBtn = "결과저장";
+        const mainBtn = "메인으로";
         return (
             <div className="result">
                 <div className="container">
-                    <div className="type">[ {type_name} ]</div>
-                    <img className="type-img" src={imgUrl} alt={type_name} />
+                    <div
+                        className="subject_name"
+                        // html 형태로 렌더링 허용
+                        dangerouslySetInnerHTML={{ __html: subject_name }}
+                    ></div>
+                    <img className="type-img" src={imgUrl} alt={subject_name} />
+                    <div
+                        className="main_context"
+                        // html 형태로 렌더링 허용
+                        dangerouslySetInnerHTML={{ __html: main_context }}
+                    ></div>
                     <div
                         className="sub_context"
                         // html 형태로 렌더링 허용
                         dangerouslySetInnerHTML={{ __html: sub_context }}
                     ></div>
+
+                    <div className="buttons">
+                        <button
+                            className="save_btn"
+                            onClick={() => this.props.history.push("/collect")}
+                        >
+                            {saveBtn}
+                        </button>
+                        <button
+                            className="main_btn"
+                            onClick={() => this.props.history.push("/")}
+                        >
+                            {mainBtn}
+                        </button>
+                    </div>
                 </div>
             </div>
         );
@@ -23,13 +51,11 @@ class result extends React.Component {
 }
 
 // 기본 props 값
-result.defaultProps = {
-    type_name: "비기너",
+result5_1.defaultProps = {
+    subject_name: "나는 몇 년차 개발자의 마인드일까?",
+    main_context: "비기너",
     sub_context:
-        "당신은 비기너 개발자의 마인드를 가지고 있습니다!" +
-        "개발을 시작한지 얼마 되지 않으셨나요?" +
-        "주니어, 시니어 개발자가 되려면 얼마 남지 않았습니다. 화이팅!"
-        
+        "당신은 비기너 개발자의 마인드를 가지고 있습니다! <br/> 개발을 시작한지 얼마 되지 않으셨나요? 주니어, 시니어 개발자가 되려면 얼마 남지 않았습니다. 화이팅!"
 };
 
-export default result;
+export default result5_1;
