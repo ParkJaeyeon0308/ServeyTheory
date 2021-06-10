f936071204c039ea387d578ee2972e40f607a3a1
suyeon <s2019w27@e-mirim.hs.kr>
2021-06-10 12:04:40 +0900

    slider img 추가 및 크기 변경

--- surveytheory/src/pages/Home/home.js
+++ surveytheory/src/pages/Home/home.js
@@ -11,8 +11,8 @@ export default class Home extends Component {
     render() {
         // img style settings
         const img_style = {
-            width: "100%",
-            height: "100%",
+            width: "100vw",
+            height: "82vh",
             cursor: "pointer"
         };
 
@@ -33,7 +33,7 @@ export default class Home extends Component {
                         onClick={() => this.props.history.push("/Question1_1")}
                     >
                         <img
-                            src="/images/slider-img/앱 vs 웹.png"
+                            src="/images/slider-img/img1_1.png"
                             style={img_style}
                         />
                     </div>
@@ -41,7 +41,7 @@ export default class Home extends Component {
                         onClick={() => this.props.history.push("/question2_1")}
                     >
                         <img
-                            src="/images/slider-img/3개월 프로젝트가 주어졌을때 내가 해낼 수 있는 기간은.png"
+                            src="/images/slider-img/img1_2.png"
                             style={img_style}
                         />
                     </div>
@@ -49,7 +49,7 @@ export default class Home extends Component {
                         onClick={() => this.props.history.push("/question3_1")}
                     >
                         <img
-                            src="/images/slider-img/정신학년테스트.png"
+                            src="/images/slider-img/img1_3.png"
                             style={img_style}
                         />
                     </div>
@@ -57,7 +57,7 @@ export default class Home extends Component {
                         onClick={() => this.props.history.push("/question4_1")}
                     >
                         <img
-                            src="/images/slider-img/직장 MBTI.png"
+                            src="/images/slider-img/img1_4.png"
                             style={img_style}
                         />
                     </div>
@@ -65,7 +65,7 @@ export default class Home extends Component {
                         onClick={() => this.props.history.push("/question5_1")}
                     >
                         <img
-                            src="/images/slider-img/나는 몇년차 개발자의 마인드일까.png"
+                            src="/images/slider-img/img1_5.png"
                             style={img_style}
                         />
                     </div>
@@ -73,7 +73,7 @@ export default class Home extends Component {
                         onClick={() => this.props.history.push("/question6_1")}
                     >
                         <img
-                            src="/images/slider-img/나와 어울리는 과는.png"
+                            src="/images/slider-img/img1_6.png"
                             style={img_style}
                         />
                     </div>
@@ -81,7 +81,7 @@ export default class Home extends Component {
                         onClick={() => this.props.history.push("/question7_1")}
                     >
                         <img
-                            src="/images/slider-img/나와 어울리는 개발자 포지션은.png"
+                            src="/images/slider-img/img1_7.png"
                             style={img_style}
                         />
                     </div>
@@ -89,7 +89,7 @@ export default class Home extends Component {
                         onClick={() => this.props.history.push("/question8_1")}
                     >
                         <img
-                            src="/images/slider-img/내가 잘 할 것같은 PG 언어는.png"
+                            src="/images/slider-img/img1_8.png"
                             style={img_style}
                         />
                     </div>
@@ -97,7 +97,7 @@ export default class Home extends Component {
                         onClick={() => this.props.history.push("/question9_1")}
                     >
                         <img
-                            src="/images/slider-img/팀플에서 나의 포지션은.png"
+                            src="/images/slider-img/img1_9.png"
                             style={img_style}
                         />
                     </div>
@@ -106,78 +106,57 @@ export default class Home extends Component {
 
                 <div
                     className="box"
-                    id="box1"
                     onClick={() => this.props.history.push("/Question1_1")}
                 >
-                    <div className="test_title">웹, 앱 뭐가 더 잘 맞을까?</div>
+                    <img src="../images/box-img/img2_1.png" />
                 </div>
                 <div
                     className="box"
-                    id="box2"
                     onClick={() => this.props.history.push("/question2_1")}
                 >
-                    <div className="test_title">
-                        3개월 프로젝트가 주어졌을때
-                        <br />
-                        내가 해낼 수 있는 기간은?
-                    </div>
+                    <img src="../images/box-img/img2_2.png" />
                 </div>
                 <div
                     className="box"
-                    id="box3"
                     onClick={() => this.props.history.push("/question3_1")}
                 >
-                    <div className="test_title">정신학년 테스트</div>
+                    <img src="../images/box-img/img2_3.png" />
                 </div>
                 <div
                     className="box"
-                    id="box4"
                     onClick={() => this.props.history.push("/question4_1")}
                 >
-                    <div className="test_title">직장 mbti 테스트</div>
+                    <img src="../images/box-img/img2_4.png" />
                 </div>
                 <div
                     className="box"
-                    id="box5"
                     onClick={() => this.props.history.push("/question5_1")}
                 >
-                    <div className="test_title">
-                        나는 몇년차 개발자의
-                        <br /> 마인드일까?
-                    </div>
+                    <img src="../images/box-img/img2_5.png" />
                 </div>
                 <div
                     className="box"
-                    id="box6"
                     onClick={() => this.props.history.push("/question6_1")}
                 >
-                    <div className="test_title">나와 어울리는 과는?</div>
+                    <img src="../images/box-img/img2_6.png" />
                 </div>
                 <div
                     className="box"
-                    id="box7"
                     onClick={() => this.props.history.push("/question7_1")}
                 >
-                    <div className="test_title">
-                        나와 어울리는 개발자 포지션은?
-                    </div>
+                    <img src="../images/box-img/img2_7.png" />
                 </div>
                 <div
                     className="box"
-                    id="box8"
                     onClick={() => this.props.history.push("/question8_1")}
                 >
-                    <div className="test_title">
-                        내가 잘할 것 같은
-                        <br /> 프로그래밍 언어는?
-                    </div>
+                    <img src="../images/box-img/img2_8.png" />
                 </div>
                 <div
                     className="box"
-                    id="box9"
                     onClick={() => this.props.history.push("/question9_1")}
                 >
-                    <div className="test_title">팀플에서 나의 포지션은?</div>
+                    <img src="../images/box-img/img2_9.png" />
                 </div>
                 <div className="text">계속 설문을 만드는 중 입니다!</div>
             </div>
